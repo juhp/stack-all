e67449ad26859725f7951fe5524d18c559266fff
Jens Petersen <petersen@redhat.com>
2020-10-28 21:16:13 +0800

    read oldest lts from .stack-all

--- src/Main.hs
+++ src/Main.hs
@@ -1,50 +1,77 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Control.Monad.Extra
+import Data.Ini.Config
 import Data.List.Extra
+import qualified Data.Text.IO as T
 import SimpleCmd
 import System.Directory
 import System.FilePath
+
+-- FIXME allow specific snapshots?
+-- FIXME lts latest
+data Snapshot = LTS Int | Nightly
+  deriving (Eq, Ord)
+
+defaultSnaps :: [Snapshot]
+defaultSnaps = [Nightly, LTS 16, LTS 14, LTS 13, LTS 12, LTS 11]
+
+readSnap :: String -> Snapshot
+readSnap "nightly" = Nightly
+readSnap snap =
+  if "lts" `isPrefixOf` snap then
+    let major = read (dropPrefix "lts-" snap) in LTS major
+  else error' $ "malformed snapshot " ++ snap
+
+showSnap :: Snapshot -> String
+showSnap Nightly = "nightly"
+showSnap (LTS ver) = "lts-" ++ show ver
 
 main :: IO ()
 main = do
   unlessM (doesFileExist "stack.yaml") $
     error' "no stack.yaml found"
   configs <- filter isStackConf <$> listDirectory "."
-  mapM_ (stackBuild configs) [Nightly, LTS 16, LTS 14, LTS 13, LTS 12]
+  moldest <- getOldestLTS
+  mapM_ (stackBuild configs) $
+    case moldest of
+      Just oldest -> filter (>= oldest) defaultSnaps
+      Nothing -> defaultSnaps
+  where
+    isStackConf :: FilePath -> Bool
+    isStackConf f = "stack-" `isPrefixOf` f && "yaml" `isExtensionOf` f
 
-isStackConf :: FilePath -> Bool
-isStackConf f = "stack-" `isPrefixOf` f && "yaml" `isExtensionOf` f
+getOldestLTS :: IO (Maybe Snapshot)
+getOldestLTS = do
+  let configFile = ".stack-all"
+  haveConfig <- doesFileExist configFile
+  if haveConfig then
+    Just . readSnap <$> readIniConfig configFile rcParser id
+    else return Nothing
+  where
+    rcParser :: IniParser String
+    rcParser =
+      section "versions" $
+      fieldOf "oldest" string
 
--- FIXME allow specific snapshots?
--- FIXME lts latest
-data Snapshot = Nightly | LTS Int
-
-showSnap :: Snapshot -> String
-showSnap Nightly = "nightly"
-showSnap (LTS ver) = "lts-" ++ show ver
-
-compactSnap :: Snapshot -> String
-compactSnap Nightly = "nightly"
-compactSnap (LTS ver) = "lts" ++ show ver
-
-snapToConfig :: Snapshot -> FilePath
-snapToConfig snap = "stack-" ++ compactSnap snap <.> "yaml"
-
--- configToSnap :: FilePath -> Snapshot
--- configToSnap f =
---   if isStackConf f then
---     let snap = takeBaseName $ dropPrefix "stack-" f
---     in if snap == "nightly" then Nightly
---        else if "lts" `isPrefixOf` snap then
---               let major = read (dropPrefix "lts" snap) in LTS major
---             else error' $ "malformed snapshot config file " ++ f
---   else error' ("malformed stack config filename " ++ f)
+    readIniConfig :: FilePath -> IniParser a -> (a -> b) -> IO b
+    readIniConfig inifile iniparser fn = do
+      ini <- T.readFile inifile
+      return $ either error fn $ parseIniFile ini iniparser
 
 stackBuild :: [FilePath] -> Snapshot -> IO ()
 stackBuild configs snap = do
   let resolver = showSnap snap
       config =
-        case sort (filter (snapToConfig snap <=) configs) of
+        case sort (filter (snapConfig <=) configs) of
           [] -> []
           (cfg:_) -> ["--stack-yaml", cfg]
   cmd_ "stack" $ ["--resolver", resolver, "build"] ++ config
   putStrLn ""
+  where
+    snapConfig :: FilePath
+    snapConfig = "stack-" ++ compactSnap snap <.> "yaml"
+      where
+        compactSnap :: Snapshot -> String
+        compactSnap Nightly = "nightly"
+        compactSnap (LTS ver) = "lts" ++ show ver
