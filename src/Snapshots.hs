b389ee79bfc1ed0e3ac98a0ba78bea65c1a6a63c
Jens Petersen <petersen@redhat.com>
2021-07-03 00:52:36 +0800

    use cached-json-file

--- src/Snapshots.hs
+++ src/Snapshots.hs
@@ -6,18 +6,12 @@ module Snapshots (
   )
 where
 
-import Control.Monad
 import Data.Aeson
-import qualified Data.ByteString.Lazy.Char8 as B
 import Data.List.Extra
 import qualified Data.HashMap.Strict as H
 import qualified Data.Text as T
-import Data.Time.Clock (diffUTCTime, getCurrentTime)
 import Network.HTTP.Query
-import SimpleCmd (error')
-import System.Directory
-import System.Environment.XDG.BaseDir
-import System.FilePath
+import System.Cached.JSON
 
 import MajorVer
 
@@ -33,28 +27,6 @@ latestSnapshot :: MajorVer -> IO (Maybe String)
 latestSnapshot ver = do
   lookupKey (T.pack (showMajor ver)) <$> getSnapshots
 
--- FIXME handle network failure
 getSnapshots :: IO Object
-getSnapshots = do
-  file <- getUserCacheFile "stack-all" "snapshots.json"
-  exists <- doesFileExist file
-  unless exists $ do
-    putStrLn $ "Creating " ++ file ++ " ..."
-    createDirectoryIfMissing True (takeDirectory file)
-  recent <- do
-    if exists then do
-      ts <- getModificationTime file
-      t <- getCurrentTime
-      -- < 3 hours
-      return $ diffUTCTime t ts < 10000
-      else return False
-  if recent
-    then do
-    eobj <- eitherDecode <$> B.readFile file
-    case eobj of
-      Left err -> error' err
-      Right obj -> return obj
-    else do
-    obj <- webAPIQuery "http://haddock.stackage.org/snapshots.json" []
-    B.writeFile file $ encode obj
-    return obj
+getSnapshots =
+  getCachedJSON "stackage-snapshots" "snapshots.json" "http://haddock.stackage.org/snapshots.json" 200
